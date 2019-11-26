9234d30ecb52c8e55a665788e50f71cc8b4761d9
Douglas Henrique <doughrqe@gmail.com>
2019-11-26 17:53:48 -0300

    2 Bonus

--- Douglas/script.js
+++ Douglas/script.js
@@ -1,13 +1,28 @@
-let botao = document.getElementById('diretorio');
-botao.addEventListener('change', function() {
-    let image = document.getElementById('pegarTag');
-    image.src = URL.createObjectURL(event.target.files[0]);
-    image.alt = "gerador de memes";
-    image.style.width = "900px";
-    image.style.height = "500px";
-});
-let texto = document.getElementById("entrada");
-texto.addEventListener('keydown', function() {
-    let escreve = document.getElementById("texto");
-    escreve.innerHTML = texto.value;
-})
+window.onload = function() {
+    let botao = document.getElementById('diretorio');
+
+    function criaImagem() {
+        let image = document.getElementById('pegarTag');
+        image.src = URL.createObjectURL(event.target.files[0]);
+        image.alt = "gerador de memes";
+        image.style.width = "900px";
+        image.style.height = "500px";
+    }
+
+    botao.addEventListener('change', function() {
+        criaImagem();
+    });
+
+    let texto = document.getElementById("entrada");
+
+    texto.addEventListener('input', function() {
+        let escreve = document.getElementById("texto");
+        escreve.innerHTML = texto.value;
+
+    })
+    let texto2 = document.getElementById("entrada2")
+    texto2.addEventListener('input', function() {
+        let escreve = document.getElementById("texto1");
+        escreve.innerHTML = texto2.value;
+    })
+}
