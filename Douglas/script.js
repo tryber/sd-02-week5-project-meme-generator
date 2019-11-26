c861f984af60c07fbbca4602c2daea49d2ee747b
Douglas Henrique <doughrqe@gmail.com>
2019-11-26 16:40:08 -0300

    basico terminado

--- Douglas/script.js
+++ Douglas/script.js
@@ -1,5 +1,13 @@
-function carregaImagem(event) {
-    let image = document.getElementById('output');
+let botao = document.getElementById('diretorio');
+botao.addEventListener('change', function() {
+    let image = document.getElementById('pegarTag');
     image.src = URL.createObjectURL(event.target.files[0]);
     image.alt = "gerador de memes";
-};
+    image.style.width = "900px";
+    image.style.height = "500px";
+});
+let texto = document.getElementById("entrada");
+texto.addEventListener('keydown', function() {
+    let escreve = document.getElementById("texto");
+    escreve.innerHTML = texto.value;
+})
