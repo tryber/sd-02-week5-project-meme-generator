879cfe0b46bc2d18ac8524d899ffe8edcc81391e
Lucas-Werner <lucas.1werner@gmail.com>
2019-11-26 19:02:44 -0300

    adicionando img

--- lucas-werner/script.js
+++ lucas-werner/script.js
@@ -1,1 +1,17 @@
-let imgSelecionada = document.getElementById("img-selecionada")
+let selecionarImagem = document.getElementById("pick-img")
+
+console.log(selecionarImagem)
+
+selecionarImagem.addEventListener ('change', function() {
+    criarImagem () })
+
+function criarImagem () {
+    
+    let imgEscolhida = document.getElementById("print-img");
+        imgEscolhida.src = URL.createObjectURL(event.target.files[0]);
+        imgEscolhida.style.width = "300px"
+        imgEscolhida.style.height = "400px"
+         
+    }
+
+ 
