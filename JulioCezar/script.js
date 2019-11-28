de0e236f40167bb4809d893a57392cda180fe177
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2019-11-28 15:47:45 -0200

    Adicionado bonus: botao que aumenta e diminui tamanho de cada texto e bota pra customizar borda

--- JulioCezar/script.js
+++ JulioCezar/script.js
@@ -8,7 +8,76 @@ window.onload = function() {
   inputUp.addEventListener('keyup', updateTextUp);
   const imagem = document.getElementById('input_img');
   imagem.addEventListener('change', adicionarImagem);
+  const buttonUp1 = document.getElementById('button_up_1');
+  buttonUp1.addEventListener('click', fontSizeUpDown1);
+  const buttonDown1 = document.getElementById('button_down_1');
+  buttonDown1.addEventListener('click', fontSizeUpDown1);
+  const buttonUp2 = document.getElementById('button_up_2');
+  buttonUp2.addEventListener('click', fontSizeUpDown2);
+  const buttonDown2 = document.getElementById('button_down_2');
+  buttonDown2.addEventListener('click', fontSizeUpDown2);
+  const buttonBorder = document.getElementById('button_border');
+  buttonBorder.addEventListener('click', mudaBorda);
+}
 
+function mudaBorda(event) {
+  const buttonStyle = getComputedStyle(document.getElementById('id_imagem_div'));
+  const buttonBorder = buttonStyle.borderStyle;
+  if (buttonBorder === 'solid'){
+    document.getElementById('id_imagem_div').style.borderStyle = 'double';
+  } else if (buttonBorder === 'dashed') {
+    document.getElementById('id_imagem_div').style.borderStyle = 'solid';
+  } else if (buttonBorder === 'double') {
+    document.getElementById('id_imagem_div').style.borderStyle = 'dashed';
+  }
+}
+
+function fontSizeUpDown1(event) {
+  if (document.getElementById('button_up_1') === event.target) {
+    const text = document.querySelector('#span_meme_text_up');
+    const style = getComputedStyle(document.querySelector('#span_meme_text_up'));
+    let tamanhoFont = parseInt(style.fontSize);
+    let teste = tamanhoFont + 5;    
+    if (teste <= 100) {
+    text.style.fontSize = teste + 'px';        
+    } else {
+      alert('texto nao aumenta mais que 100px');
+    }
+  } else {
+    const text = document.querySelector('#span_meme_text_up');
+    const style = getComputedStyle(document.querySelector('#span_meme_text_up'));
+    let tamanhoFont = parseInt(style.fontSize);
+    let teste = tamanhoFont - 5;    
+    if (teste >= 20) {
+      text.style.fontSize = teste + 'px';
+    } else {
+      alert ('texto nao diminui mais que 10px');
+    }
+  }
+}
+
+function fontSizeUpDown2() {
+  if (document.getElementById('button_up_2') === event.target) {
+    const text = document.querySelector('#span_meme_text_down');
+    const style = getComputedStyle(document.querySelector('#span_meme_text_down'));
+    let tamanhoFont = parseInt(style.fontSize);
+    let teste = tamanhoFont + 5;    
+    if (teste <= 100) {
+    text.style.fontSize = teste + 'px';        
+    } else {
+      alert('texto nao aumenta mais que 100px');
+    }
+  } else {
+    const text = document.querySelector('#span_meme_text_down');
+    const style = getComputedStyle(document.querySelector('#span_meme_text_down'));
+    let tamanhoFont = parseInt(style.fontSize);
+    let teste = tamanhoFont - 5;    
+    if (teste >= 20) {
+      text.style.fontSize = teste + 'px';
+    } else {
+      alert ('texto nao diminui mais que 10px');
+    }
+  }
 }
 
 function updateTextDown() {
