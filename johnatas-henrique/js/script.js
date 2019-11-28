e7bc845e6e79be521456c2ce84618dac653998f3
Johnatas Henrique <johnatas.henrique@gmail.com>
2019-11-28 01:20:49 -0300

    Bug Squash Anterior: 136 faltando

--- johnatas-henrique/js/script.js
+++ johnatas-henrique/js/script.js
@@ -1,16 +1,20 @@
-divTextoBaixo = document.querySelector('.texto-baixo');
-carregarTexto = document.querySelector('.caixa-texto-input');
-recebeTexto = document.createElement('p');
+const divTextoBaixo = document.querySelector('.texto-baixo');
+const carregarTexto = document.querySelector('.caixa-texto-input');
+const recebeTexto = document.createElement('p');
 recebeTexto.classList += 'texto-meme-baixo';
 divTextoBaixo.appendChild(recebeTexto);
 
-carregarTexto.addEventListener('keyup', function() {
-    recebeTexto.innerHTML = carregarTexto.value;
-});
+carregarTexto.addEventListener('keyup', loadText);
 
-recebeFoto = document.getElementById('img-foto');
-botaoFoto = document.getElementById('botao-foto');
+function loadText() {
+  recebeTexto.innerHTML = carregarTexto.value;
+}
 
-botaoFoto.addEventListener('change', function(event) {
-    recebeFoto.src = URL.createObjectURL(event.target.files[0]);
-});
+const recebeFoto = document.getElementById('img-foto');
+const botaoFoto = document.getElementById('botao-foto');
+
+botaoFoto.addEventListener('change', loadImage);
+
+function loadImage(event) {
+  recebeFoto.src = URL.createObjectURL(event.target.files[0]);
+}
