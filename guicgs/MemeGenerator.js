2ec6a8bb203ff6ea8f9e43666193de0c4d1c05f3
guicgs <etohlif@protonmail.com>
2019-11-28 02:25:37 -0300

    algumas correções codeclimate JS

--- guicgs/MemeGenerator.js
+++ guicgs/MemeGenerator.js
@@ -2,20 +2,20 @@
 // inputSelecaoImagem.style.opacity = 0;
 // inputSelecaoImagem.addEventListener("change", updateImageDisplay);
 
-let imagemEscolhida = document.getElementById("imagem");
-imagemEscolhida.addEventListener("change", function(event) {
-    let imagemUpload = document.getElementById("imagemSelecionada");
-    imagemUpload.src = URL.createObjectURL(event.target.files[0]);
+const imagemEscolhida = document.getElementById('imagem');
+imagemEscolhida.addEventListener('change', function (event) {
+  const imagemUpload = document.getElementById('imagemSelecionada');
+  imagemUpload.src = URL.createObjectURL(event.target.files[0]);
 });
 
-let textoSuperior = document.getElementsByClassName("inputTexto")[0];
-textoSuperior.addEventListener("keyup", function() {
-    let imprimirTexto = document.getElementById("textoCima");
-    imprimirTexto.innerHTML = textoSuperior.value;
+const textoSuperior = document.getElementsByClassName('inputTexto')[0];
+textoSuperior.addEventListener('keyup', function () {
+  const imprimirTexto = document.getElementById('textoCima');
+  imprimirTexto.innerHTML = textoSuperior.value;
 });
 
-let textoInferior = document.getElementsByClassName("inputTexto")[1];
-textoInferior.addEventListener("keyup", function() {
-    let imprimirTexto = document.getElementById("textoBaixo");
-    imprimirTexto.innerHTML = textoInferior.value;
+const textoInferior = document.getElementsByClassName('inputTexto')[1];
+textoInferior.addEventListener('keyup', function () {
+  const imprimirTexto = document.getElementById('textoBaixo');
+  imprimirTexto.innerHTML = textoInferior.value;
 });
