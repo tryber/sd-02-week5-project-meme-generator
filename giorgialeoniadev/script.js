358b6a3d195920b5e39a1b5fe647ede5429caf5a
Giorgia <giorgialeoniac@gmail.com>
2019-11-29 15:33:27 -0300

    Aprimoração na estilização da página e no funcionamento dos scripts, iniciação no javascript já com auxílio da monitoria com o instrutor Lucas no dia 29 de Novembro

--- giorgialeoniadev/script.js
+++ giorgialeoniadev/script.js
@@ -1,12 +1,20 @@
-// Primeiro eu clico na imagem
-
 //Funções
-printar()
+printar();
 
 //Variáveis
 let resultado = document.querySelector("#output")
+let imguser = document.querySelector("#file")
+let textoSuperior = document.getElementById("textocima")
+let textoInferior = document.getElementById("textobaixo")
+let textoSuperiorFinal = document.getElementById("textofinalcima")
+let textoInferiorFinal = document.getElementById("textofinalbaixo")
 
-//Clico na imagem e coloco ela na moldura
+//Se o usuário upar uma imagem do computador
+imguser.addEventListener('change', function (event){
+    resultado.src = URL.createObjectURL(event.target.files[0]);
+});
+
+//Se o usuário escolher uma das imagens ofertadas na página
 function printar() {
     let img = document.getElementsByClassName("imgcarregadas");
     console.log(img)
@@ -19,12 +27,11 @@ function printar() {
     })
 }
 
+//Usuário inserindo texto sobre a imagem
+textoSuperior.addEventListener('keyup', function () {
+   textoSuperiorFinal.innerHTML = textoSuperior.value;
+})
 
-function adicionandoImagem() {
-    var img = document.getElementById("file")
-}
-
-function selecionaPopulares(){
-    var populares = document.querySelector(".imgcarregadas")
-    
-}
+textoInferior.addEventListener('keyup', function () {
+    textoInferiorFinal.innerHTML = textoInferior.value;
+})
