6dfe5c8b822d2286344694d6d1de8b2b1d81ad52
EddyeBoy27 <lipe0503@gmail.com>
2019-11-29 18:44:48 -0200

    Style Issues Fix

--- Felipe/script.js
+++ Felipe/script.js
@@ -1,13 +1,11 @@
 const textoForm = document.querySelector('.caixaTexto');
 const textoCaixa = document.querySelector('.textoNaImagem');
 
-
 function colocaTextoCaixa() {
   textoCaixa.innerText = textoForm.value;
 }
 
 textoForm.addEventListener('keyup', colocaTextoCaixa);
-
 
 const recebeFoto = document.querySelector('.imagemUnica');
 const botaoFoto = document.querySelector('#botaoFoto');
@@ -15,5 +13,4 @@ const botaoFoto = document.querySelector('#botaoFoto');
 function loadImage(event) {
   recebeFoto.src = window.URL.createObjectURL(event.target.files[0]);
 }
-
 botaoFoto.addEventListener('change', loadImage);
