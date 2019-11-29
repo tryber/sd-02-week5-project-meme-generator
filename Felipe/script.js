37bc1c5fc180fe2d05f4a7040f494c7ea3f02da8
EddyeBoy27 <lipe0503@gmail.com>
2019-11-29 17:54:54 -0200

    Script Issues Codeclimate FIX

--- Felipe/script.js
+++ Felipe/script.js
@@ -1,23 +1,19 @@
-/* Insere texto do formulário na imagem */
-
 const textoForm = document.querySelector('.caixaTexto');
 const textoCaixa = document.querySelector('.textoNaImagem');
 
 
 function colocaTextoCaixa() {
-    textoCaixa.innerText = textoForm.value;
+textoCaixa.innerText = textoForm.value;
 }
 
 textoForm.addEventListener('keyup', colocaTextoCaixa);
 
 
-/* Insere imagem no img */
-
 const recebeFoto = document.querySelector('.imagemUnica');
 const botaoFoto = document.querySelector('#botaoFoto');
 
 function loadImage(event) {
-    recebeFoto.src = window.URL.createObjectURL(event.target.files[0]);
+recebeFoto.src = window.URL.createObjectURL(event.target.files[0]);
 }
 
 botaoFoto.addEventListener('change', loadImage);
