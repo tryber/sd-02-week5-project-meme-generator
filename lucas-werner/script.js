089f042eb904444ee7f2e1307914019464708524
Lucas-Werner <lucas.1werner@gmail.com>
2019-12-02 15:23:59 -0300

    será que deu bom no climate?

--- lucas-werner/script.js
+++ lucas-werner/script.js
@@ -1,35 +1,40 @@
-let selecionarImagem = document.getElementById("pick-img");
-selecionarImagem.addEventListener ('change', criarImagem);
-
-function criarImagem () {
-    
-    let imgEscolhida = document.getElementById("print-img");
-        console.log(event.target.files[0]);
-        imgEscolhida.src = URL.createObjectURL(event.target.files[0]);
-        imgEscolhida.style.width = "450px";
-        imgEscolhida.style.height = "450px";
-         
-    }
-
-
-let selecionarTextoCima = document.getElementById("insert-upper-txt");
-   
-selecionarTextoCima.addEventListener ('input', function() {
-    criarTextoCima() })
-
+const selecionarImagem = document.getElementById('pick-img');
+const selecionarTextoCima = document.getElementById('insert-upper-txt');
+const selecionarTextoBaixo = document.getElementById("insert-lower-txt");
+let selecionarTexto = document.getElementsByClassName('meme-txt');
+let imgEscolhida = document.getElementById('print-img');
+let clickUpsize = document.getElementById('upsize');
+let clickDownsize = document.getElementById('downsize');
+selecionarTexto[0].style.fontSize = '40px';
+selecionarTexto[1].style.fontSize = '40px';
+  
+function criarImagem (){    
+  imgEscolhida.src = URL.createObjectURL(event.target.files[0]);
+  imgEscolhida.style.width = '450px';
+  imgEscolhida.style.height = '450px';
+};
 function criarTextoCima () {
-    
-   document.getElementById("upper-txt").innerHTML = selecionarTextoCima.value
-
-}
-
-let selecionarTextoBaixo = document.getElementById("insert-lower-txt");
-   
-selecionarTextoBaixo.addEventListener ('input', function() {
-    criarTextoBaixo() })
-
+  document.getElementById('upper-txt').innerHTML = selecionarTextoCima.value;
+};
 function criarTextoBaixo () {
-    
-   document.getElementById("lower-txt").innerHTML = selecionarTextoBaixo.value
-
+  document.getElementById('lower-txt').innerHTML = selecionarTextoBaixo.value;
+};
+function tamanhoLetra() {
+  if (this.id == 'upsize') {
+      if (selecionarTexto[0].offsetHeight < 380 && selecionarTexto[1].offsetHeight < 380) {
+          selecionarTexto[0].style.fontSize = (parseInt(selecionarTexto[0].style.fontSize) + 5 + 'px');
+          selecionarTexto[1].style.fontSize = (parseInt(selecionarTexto[1].style.fontSize) + 5 + 'px');
+      }
+  };
+  if (this.id == 'downsize') {
+      selecionarTexto[0].style.fontSize = (parseInt(selecionarTexto[0].style.fontSize) - 5 + 'px');
+      selecionarTexto[1].style.fontSize = (parseInt(selecionarTexto[1].style.fontSize) - 5 + 'px');
+  }
 }
+selecionarImagem.addEventListener('change',criarImagem); 
+selecionarTextoCima.addEventListener('input',function ()    {criarTextoCima ()
+});   
+selecionarTextoBaixo.addEventListener ('input', function()  {criarTextoBaixo ()
+});
+clickDownsize.addEventListener('click', tamanhoLetra);
+clickUpsize.addEventListener('click', tamanhoLetra);
