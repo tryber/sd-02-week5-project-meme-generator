83e0369b7b27e1d4f9fc664801a6a25bbdb9a34f
Andrey <andreytsuzuki@gmail.com>
2019-12-02 18:08:11 -0300

    ojfs

--- andrey/script.js
+++ andrey/script.js
@@ -1,134 +1,144 @@
 const inputUp = document.querySelector('.up textarea');
-      const textUp = document.querySelector('p.textUp');
+const textUp = document.querySelector('p.textUp');
 
-      const inputDown = document.querySelector('.down textarea');
-      const textDown = document.querySelector('p.textDown');
+const inputDown = document.querySelector('.down textarea');
+const textDown = document.querySelector('p.textDown');
 
-      const image = document.querySelector('.meme img');
-      const opcoes = document.querySelector('.opcoes');
+const image = document.querySelector('.meme img');
+const opcoes = document.querySelector('.opcoes');
 
-      const dropArea = document.querySelector('.dropArea');
+const dropArea = document.querySelector('.dropArea');
 
-      const btnReset = document.querySelector('button.reset');
-      const btnResetSpan = document.querySelector('button.reset span');
-      const btnResetIcon = document.querySelector('button.reset .material-icons');
-      const icones = document.querySelectorAll('.optionsText i');
-      const selecionado = [];
+const btnReset = document.querySelector('button.reset');
+const btnResetSpan = document.querySelector('button.reset span');
+const btnResetIcon = document.querySelector('button.reset .material-icons');
+const icones = document.querySelectorAll('.optionsText i');
+const selecionado = [];
 
-      const moldura = document.querySelectorAll('.moldura .material-icons');
+const moldura = document.querySelectorAll('.moldura .material-icons');
 
-      moldura.forEach((elem) => {
-        elem.addEventListener('click', (e) => {
-          if(e.target.style.color == 'black') { image.className = ''; image.classList.add('borderBlack'); }
-          if(e.target.style.color == 'white') { image.className = ''; image.classList.add('borderWhite'); }
-          if(e.target.style.color == 'rgb(7, 39, 66)') { image.className = ''; image.classList.add('borderLess'); }
-        })
-      })
+function condicionMoldura(condicao , border) {
+  if(condicao) { image.className = ''; image.classList.add(border); }  
+}
 
-      inputUp.addEventListener('input', (e) => {
-        textUp.innerText = inputUp.value;
-      });
+moldura.forEach((elem) => {
+  elem.addEventListener('click', (e) => {
+    condicionMoldura(e.target.style.color == 'black', 'borderBlack');
+    condicionMoldura(e.target.style.color == 'white', 'borderWhite');
+    condicionMoldura(e.target.style.color == 'rgb(7, 39, 66)', 'borderLess');
+  });
+});
 
-      inputDown.addEventListener('input', (e) => {
-        textDown.innerText = inputDown.value;
-      });
+inputUp.addEventListener('input', (e) => {
+  textUp.innerText = inputUp.value;
+});
 
-      ;[textUp, textDown].forEach((elem) => {
-        ;['mouseover','mouseleave','click'].forEach((eventName) => {
-            elem.addEventListener(eventName, (e) => {
-              if(e.type == 'mouseover') {e.target.style.backgroundColor = 'black';} 
-              if(e.type == 'mouseleave') {e.target.style.backgroundColor = 'transparent';} 
-              if(e.type == 'click') { selecionado.pop(); selecionado.push(e.target); }
-          });
-        });
-      });
+inputDown.addEventListener('input', (e) => {
+  textDown.innerText = inputDown.value;
+});
 
-      icones.forEach((elem) => {
-        elem.addEventListener('click', (e) => {
-          if(selecionado[0] !== undefined){
-            let size = selecionado[0].style.fontSize;
-          switch(elem.innerText) {
-            case 'exposure_neg_1':
-              selecionado[0].style.fontSize = `${parseInt(size.slice(0, size.length-2)) - 1}px`;
-              break;
-            case 'exposure_plus_1':
-              selecionado[0].style.fontSize = `${parseInt(size.slice(0, size.length-2)) + 1}px`;
-              break;
-            case 'format_bold':
-              console.log(selecionado[0].style.fontWeight == 'normal')
-              selecionado[0].style.fontWeight == 'normal' ? selecionado[0].style.fontWeight = 'bold' : selecionado[0].style.fontWeight = 'normal';
-              break;  
-            case 'format_align_center':
-              selecionado[0].style.textAlign = 'center';
-              break; 
-            case 'format_align_left':
-              selecionado[0].style.textAlign = 'left';
-              break; 
-            case 'format_align_right':
-              selecionado[0].style.textAlign = 'right';
-              break;    
-            default:
-              console.log();
-            }
-          }
-      });
-      })
+function condicaoTextUpDown(condicao, color) {
+  if(condicao) {e.target.style.backgroundColor = color;}
+}
 
-      opcoes.querySelectorAll('figure').forEach((item) => {
-        item.addEventListener('click', (e) => {
-          image.setAttribute('src', `${e.target.getAttribute('src')}`);
-        });
-      });
+;[textUp, textDown].forEach((elem) => {
+  ;['mouseover','mouseleave','click'].forEach((eventName) => {
+      elem.addEventListener(eventName, (e) => {
+        condicaoTextUpDown(e.type == 'mouseover', 'black');
+        condicaoTextUpDown(e.type == 'mouseleave', 'transparent');
+        if(e.type == 'click') { selecionado.pop(); selecionado.push(e.target); }
+    });
+  });
+});
 
-      btnReset.addEventListener('mouseover', (e) => {
-        btnReset.style.backgroundColor = '#072742';
-        btnResetSpan.style.color = 'white';
-        btnResetIcon.style.color = 'white';
-      });
-
-      btnReset.addEventListener('mouseleave', (e) => {
-        btnReset.style.backgroundColor = 'white';
-        btnResetSpan.style.color = '#072742';
-        btnResetIcon.style.color = '#072742';
-      });
-
-      ;['mouseover','dragenter', 'dragover'].forEach((eventName) => {
-        dropArea.addEventListener(eventName, highlight, false)
-      })
-
-      ;['mouseleave','dragleave', 'drop'].forEach((eventName) => {
-        dropArea.addEventListener(eventName, unhighlight, false)
-      })
-
-      function highlight() {
-        dropArea.classList.add('highlight');
+icones.forEach((elem) => {
+  elem.addEventListener('click', (e) => {
+    if(selecionado[0] !== undefined){
+      let size = selecionado[0].style.fontSize;
+    switch(elem.innerText) {
+      case 'exposure_neg_1':
+        selecionado[0].style.fontSize = `${parseInt(size.slice(0, size.length-2)) - 1}px`;
+        break;
+      case 'exposure_plus_1':
+        selecionado[0].style.fontSize = `${parseInt(size.slice(0, size.length-2)) + 1}px`;
+        break;
+      case 'format_bold':
+        console.log(selecionado[0].style.fontWeight == 'normal')
+        selecionado[0].style.fontWeight == 'normal' ? selecionado[0].style.fontWeight = 'bold' : selecionado[0].style.fontWeight = 'normal';
+        break;  
+      case 'format_align_center':
+        selecionado[0].style.textAlign = 'center';
+        break; 
+      case 'format_align_left':
+        selecionado[0].style.textAlign = 'left';
+        break; 
+      case 'format_align_right':
+        selecionado[0].style.textAlign = 'right';
+        break;    
+      default:
+        console.log();
       }
+    }
+});
+});
 
-      function unhighlight() {
-        dropArea.classList.remove('highlight');
-      }
+opcoes.querySelectorAll('figure').forEach((item) => {
+  item.addEventListener('click', (e) => {
+    image.setAttribute('src', `${e.target.getAttribute('src')}`);
+  });
+});
 
-      function reset() {
-        textUp.style.fontSize = '40px';
-        textUp.style.fontWeight = 'normal';
-        textUp.style.textAlign = 'center';
-        textDown.style.fontSize = '40px';
-        textDown.style.fontWeight = 'normal';
-        textDown.style.textAlign = 'center';
-      }
+function styelResetBtn( color1, color2) {
+  btnReset.style.backgroundColor = color1;
+  btnResetSpan.style.color = color2;
+  btnResetIcon.style.color = color2;
+}
 
-      function previewFile() {
-        const preview = document.querySelector('.meme img');
-        const file = document.querySelector('input[type=file]').files[0];
-        const reader = new FileReader();
+btnReset.addEventListener('mouseleave', styelResetBtn('white', '#072742'));
+btnReset.addEventListener('mouseover', styelResetBtn('#072742', 'white'));
+      
+function dropAreaAddEventListener(eventName, param){
+  dropArea.addEventListener(eventName, param, false);
+}
 
-        reader.onloadend = function () {
-          preview.src = reader.result;
-        }
+;['mouseover','dragenter', 'dragover'].forEach((eventName) => {
+  dropAreaAddEventListener(eventName, highlight);
+});
 
-        if (file) {
-          reader.readAsDataURL(file);
-        } else {
-          preview.src = '';
-        }
-      }
+;['mouseleave','dragleave', 'drop'].forEach((eventName) => {
+  dropAreaAddEventListener(eventName, unhighlight);
+});
+
+
+function highlight() {
+  dropArea.classList.add('highlight');
+}
+
+function unhighlight() {
+  dropArea.classList.remove('highlight');
+}
+
+function reset() {
+  textUp.style.fontSize = '40px';
+  textUp.style.fontWeight = 'normal';
+  textUp.style.textAlign = 'center';
+  textDown.style.fontSize = '40px';
+  textDown.style.fontWeight = 'normal';
+  textDown.style.textAlign = 'center';
+}
+
+function previewFile() {
+  const preview = document.querySelector('.meme img');
+  const file = document.querySelector('input[type=file]').files[0];
+  const reader = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = '';
+  }
+}
