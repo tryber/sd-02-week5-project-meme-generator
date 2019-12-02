a477a8d58411f6a9e9c8c805ceb27dc4e92a70b1
Giorgia <giorgialeoniac@gmail.com>
2019-12-02 16:36:09 -0300

    Correção do code climate

--- giorgialeoniadev/script.js
+++ giorgialeoniadev/script.js
@@ -1,37 +1,35 @@
-//Funções
-printar();
+// Variáveis
+const resultado = document.querySelector('#output');
+const imguser = document.querySelector('#file');
+const textoSuperior = document.getElementById('textocima');
+const textoInferior = document.getElementById('textobaixo');
+const textoSuperiorFinal = document.getElementById('textofinalcima');
+const textoInferiorFinal = document.getElementById('textofinalbaixo');
 
-//Variáveis
-let resultado = document.querySelector("#output")
-let imguser = document.querySelector("#file")
-let textoSuperior = document.getElementById("textocima")
-let textoInferior = document.getElementById("textobaixo")
-let textoSuperiorFinal = document.getElementById("textofinalcima")
-let textoInferiorFinal = document.getElementById("textofinalbaixo")
-
-//Se o usuário upar uma imagem do computador
-imguser.addEventListener('change', function (event){
+// Se o usuário upar uma imagem do computador
+imguser.addEventListener('change', function (event) {
     resultado.src = URL.createObjectURL(event.target.files[0]);
 });
 
-//Se o usuário escolher uma das imagens ofertadas na página
+// Se o usuário escolher uma das imagens ofertadas na página
 function printar() {
-    let img = document.getElementsByClassName("imgcarregadas");
+    const img = document.getElementsByClassName('imgcarregadas');
     console.log(img)
-    for(let click of img) 
-    click.addEventListener('click', function (event) {
-        console.log(event)
-        console.log(event.target)
-        resultado.src = event.target.src
-    
-    })
+    for (const click of img) {
+        click.addEventListener('click', function (event) {
+            resultado.src = event.target.src
+        });
+    }
 }
 
-//Usuário inserindo texto sobre a imagem
+// Usuário inserindo texto sobre a imagem
 textoSuperior.addEventListener('keyup', function () {
-   textoSuperiorFinal.innerHTML = textoSuperior.value;
-})
+    textoSuperiorFinal.innerHTML = textoSuperior.value;
+});
 
 textoInferior.addEventListener('keyup', function () {
     textoInferiorFinal.innerHTML = textoInferior.value;
-})
+});
+
+// Funções
+printar();
