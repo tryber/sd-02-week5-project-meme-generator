6fee06b61c960884b2f94c033456b2d95a484975
Diego Augusto <contactdiegonow@gmail.com>
2020-01-20 09:12:19 -0300

    finalização meme generator beta

--- diego-august/js/main.js
+++ diego-august/js/main.js
@@ -1,165 +1,193 @@
-   var canvas;
-   var canvasWidth;
-   var ctx;
-   var x;
-   var y;
-   var download;
-   var data;
-   var fileInput;
-   var img;
+(function(window, document) {
 
+    CanvasRenderingContext2D.prototype.drawBreakingText = function(str, x, y, w, lh, method) {
+        var textSize = parseInt(this.font.replace(/\D/gi, ''));
+        var textParts = [];
+        var textPartsNo = 0;
+        var words = [];
+        var currLine = '';
+        var testLine = '';
+        str = str || '';
+        x = x || 0;
+        y = y || 0;
+        w = w || this.canvas.width;
+        lh = lh || 1;
+        method = method || 'fill';
 
-  window.onload = function() {
+        textParts = str.split('\n');
+        textPartsNo = textParts.length;
 
-
-    prepareExample();
-  }
-
-  function prepareExample() {
-    img = document.getElementById('imgg');
-
-    var deviceWidth = window.innerWidth;;
-      canvasWidth = Math.min(600, deviceWidth-20);
-  canvasHeight = Math.min(480, deviceWidth-20);
-    canvas = document.getElementById('myCanvas');
-  canvas.width = canvasWidth;
-  canvas.height = canvasHeight;
-
-
-    ctx = canvas.getContext('2d');
-
-     x = canvas.width/2 - img.width/2;
-     y = canvas.height/2 - img.height/2;
-
-    ctx.drawImage(img, x, y);
-
-    ctx.textAlign = 'center';
-    ctx.lineWidth  = 10;
-    ctx.font = '20pt impact';
-    ctx.strokeStyle = 'black';
-    ctx.fillStyle = 'white';
-    ctx.lineJoin = 'round';
-    doTransform();
-
-
-    fileInput = document.getElementById('fileInput');
-
-  fileInput.addEventListener('change', function(e) {
-
-    var reader = new FileReader();
-    reader.onload = function(event){
-
-        img.onload = function(){
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            document.getElementById('scale').value = 1;
-            document.getElementById('rotate').value = 0;
-                 x = canvas.width/2 - img.width/2;
-     y = canvas.height/2 - img.height/2;
-            ctx.drawImage(img,x,y);
+        for (var i = 0; i < textParts.length; i++) {
+            words[i] = textParts[i].split(' ');
         }
-        img.src = reader.result;
+
+        textParts = [];
+
+        for (var i = 0; i < textPartsNo; i++) {
+
+            currLine = '';
+
+            for (var j = 0; j < words[i].length; j++) {
+                testLine = currLine + words[i][j] + ' ';
+
+                if (this.measureText(testLine).width > w && j > 0) {
+                    textParts.push(currLine);
+                    currLine = words[i][j] + ' ';
+                } else {
+                    currLine = testLine;
+                }
+            }
+            textParts.push(currLine);
+        }
+
+        for (var i = 0; i < textParts.length; i++) {
+            if (method === 'fill') {
+                this.fillText(textParts[i].replace(/((\s*\S+)*)\s*/, '$1'), x, y + (textSize * lh * i));
+            } else if (method === 'stroke') {
+                this.strokeText(textParts[i].replace(/((\s*\S+)*)\s*/, '$1'), x, y + (textSize * lh * i));
+            } else if (method === 'none') {
+                return { 'textParts': textParts, 'textHeight': textSize * lh * textParts.length };
+            } else {
+                console.warn('drawBreakingText: ' + method + 'Text() does not exist');
+                return false;
+            }
+        }
+
+        return { 'textParts': textParts, 'textHeight': textSize * lh * textParts.length };
+    };
+})(window, document);
+
+
+
+
+
+var canvas = document.createElement('canvas');
+var canvasWrapper = document.getElementById('canvasWrapper');
+canvasWrapper.appendChild(canvas);
+canvas.width = 500;
+canvas.height = 500;
+var ctx = canvas.getContext('2d');
+var padding = 15;
+var textTop = 'Eu vo mata';
+var textBottom = 'O javascripto';
+var textSizeTop = 10;
+var textSizeBottom = 10;
+var image = document.createElement('img');
+
+
+
+
+
+image.onload = function(ev) {
+    canvas.outerHTML = '';
+    canvas = document.createElement('canvas');
+    canvasWrapper.appendChild(canvas);
+    ctx = canvas.getContext('2d');
+    document.getElementById('trueSize').click();
+    document.getElementById('trueSize').click();
+
+    draw();
+};
+
+document.getElementById('imgURL').oninput = function(ev) {
+    image.src = this.value;
+};
+
+document.getElementById('imgFile').onchange = function(ev) {
+    var reader = new FileReader();
+    reader.onload = function(ev) {
+        image.src = reader.result;
+    };
+    reader.readAsDataURL(this.files[0]);
+};
+
+
+
+document.getElementById('textTop').oninput = function(ev) {
+    textTop = this.value;
+    draw();
+};
+
+document.getElementById('textBottom').oninput = function(ev) {
+    textBottom = this.value;
+    draw();
+};
+
+
+
+document.getElementById('textSizeTop').oninput = function(ev) {
+    textSizeTop = parseInt(this.value);
+    draw();
+    document.getElementById('textSizeTopOut').innerHTML = this.value;
+};
+document.getElementById('textSizeBottom').oninput = function(ev) {
+    textSizeBottom = parseInt(this.value);
+    draw();
+    document.getElementById('textSizeBottomOut').innerHTML = this.value;
+};
+
+
+
+document.getElementById('trueSize').onchange = function(ev) {
+    if (document.getElementById('trueSize').checked) {
+        canvas.classList.remove('fullwidth');
+    } else {
+        canvas.classList.add('fullwidth');
     }
-    reader.readAsDataURL(fileInput.files[0]);
+};
 
 
 
+document.getElementById('export').onclick = function() {
+    var img = canvas.toDataURL('image/png');
+    var link = document.createElement("a");
+    link.download = 'Meu download';
+    link.href = img;
+    link.click();
 
-   }, false);
-
-   var controls = document.getElementById('controls');
-   var save = document.getElementById('save');
-   save.addEventListener('click', function(e) {
-     controls.style.display = 'none';
-     document.getElementById('spinner-div').style.display='inline';
-     var data = canvas.toDataURL();
-
-     request = $.ajax({
-          url: "/meme/save",
-          type: "post",
-          data: data
-      });
-
-      request.done(function (response, textStatus, jqXHR){
-          window.location.href = '/meme/view/'+response;
-      });
-    }, false);
-
-    scale = document.getElementById('scale');
-    scale.addEventListener('change', doTransform, false);
-
-    rotate = document.getElementById('rotate');
-    rotate.addEventListener('change', doTransform, false);
-
-    download = document.getElementById('img-download');
-    download.addEventListener('click', prepareDownload, false);
+    var win = window.open('', '_blank');
+    win.document.write('<img style="box-shadow: 0 0 1em 0 dimgrey;" src="' + img + '"/>');
+    win.document.write('<h1 style="font-family: Helvetica; font-weight: 300">Right Click > Save As<h1>');
+    win.document.body.style.padding = '1em';
+};
 
 
-
-  }
-
-  function doTransform() {
-    ctx.save();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.translate(canvas.width/2, canvas.height/2);
-
-    var val = document.getElementById('scale').value;
-    ctx.scale(val,val);
-
-    val = document.getElementById('rotate').value;
-    ctx.rotate(val*Math.PI/180);
-
-    ctx.translate(-canvas.width/2, -canvas.height/2);
-
-    ctx.drawImage(img, x, y);
-
-    ctx.restore();
-
-    text = document.getElementById('custom-text').value;
-     text = text.toUpperCase();
-     wrapText(ctx, text, canvas.width/2, canvas.height - canvas.height/4.5, canvasWidth-canvasWidth/3, 30);
-  }
-
-
-  function prepareDownload() {
-    var data = canvas.toDataURL();
-    download.href = data;
-  }
-
-
-  function wrapText(ctx, text, x, y, maxWidth, lineHeight) {
-    var words = text.split(' ');
-    var line = '';
-
-    for(var n = 0; n < words.length; n++) { var testLine = line + words[n] + ' '; var metrics = ctx.measureText(testLine); var testWidth = metrics.width; if (testWidth > maxWidth && n > 0) {
-        ctx.strokeText(line, x, y);
-        ctx.fillText(line, x, y);
-        line = words[n] + ' ';
-        y += lineHeight;
-      }
-      else {
-        line = testLine;
-      }
-    }
-    ctx.strokeText(line, x, y);
-    ctx.fillText(line, x, y);
-  }
-
-  function dlCanvas(eldllink) {
-      var canvas = document.querySelector('.myCanvas');
-
-      var dt = canvas.toDataURL('image/png');
-      dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
-
-      dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=meme.png');
-
-      eldllink.href = dt;
-  }
-
-  function previewText() {
-  console.log("teste de texto")
-  var input1 = document.getElementById('textInputBox1');
-  render.font = "30pt Calibri"
-  render.textAlign = "left"
-  render.fillText(input1.value, 300, 20);
+function style(font, size, align, base) {
+    ctx.font = size + 'px ' + font;
+    ctx.textAlign = align;
+    ctx.textBaseline = base;
 }
+
+function draw() {
+    // deixar em uppercase
+    var top = textTop.toUpperCase();
+    var bottom = textBottom.toUpperCase();
+
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = '#fff';
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = canvas.width * 0.001;
+
+    var _textSizeTop = textSizeTop / 100 * canvas.width;
+    var _textSizeBottom = textSizeBottom / 100 * canvas.width;
+
+    style('Impact', _textSizeTop, 'center', 'bottom');
+    ctx.drawBreakingText(top, canvas.width / 2, _textSizeTop + padding, null, 1, 'fill');
+    ctx.drawBreakingText(top, canvas.width / 2, _textSizeTop + padding, null, 1, 'stroke');
+
+    style('Impact', _textSizeBottom, 'center', 'top');
+    var height = ctx.drawBreakingText(bottom, 0, 0, null, 1, 'none').textHeight;
+    console.log(ctx.drawBreakingText(bottom, 0, 0, null, 1, 'none'));
+    ctx.drawBreakingText(bottom, canvas.width / 2, canvas.height - padding - height, null, 1, 'fill');
+    ctx.drawBreakingText(bottom, canvas.width / 2, canvas.height - padding - height, null, 1, 'stroke');
+}
+
+image.src = 'https://i.pinimg.com/originals/c2/85/24/c28524b0bbc6ef3a9266187462dc5e6c.jpg';
+document.getElementById('textSizeTop').value = textSizeTop;
+document.getElementById('textSizeBottom').value = textSizeBottom;
+document.getElementById('textSizeTopOut').innerHTML = textSizeTop;
+document.getElementById('textSizeBottomOut').innerHTML = textSizeBottom;
